53290feb6d2eefa2b7e077be1b7857297a598313
Videk3 <vid.berlocnik2@scv.si>
2023-05-19 10:56:27 +0200

    Fixed formatting of CreateBlog site

--- src/pages/CreateBlog.tsx
+++ src/pages/CreateBlog.tsx
@@ -1,57 +1,67 @@
 import {Simulate} from "react-dom/test-utils";
 import submit = Simulate.submit;
 import {SyntheticEvent, useState} from "react";
+import axios from "axios";
 
 const CreateBlog = () => {
 
-    const[title, setTitle] = useState('');
-    const[content, setContent] = useState('');
-    const[errorText, setErrorText] = useState('');
-    const[redirect, setRedirect] = useState(false);
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [category_id, setCategory] = useState(7);
+
+    const [errorText, setErrorText] = useState('');
+    const [redirect, setRedirect] = useState(false);
+
+    const style = {
+        height: '100%'
+    }
 
     const submit = async (e: SyntheticEvent) => {
         e.preventDefault();
 
-        /*const data = {
+        const data = {
             title,
-            content
+            content,
+            category_id
         };
         console.log(data);
         const res = await axios.post('http://localhost:3000/blogs', data, {withCredentials: true});
         console.log(res);
 
-        if(res.status == 201) {
-            setRedirect(true);
-        }
-        if(res.status != 201){
-            setErrorText('Napaka v podatkih');
-        }*/
-    }
+        /*     if(res.status == 201) {
+                 setRedirect(true);
+             }
+             if(res.status != 201){
+                 setErrorText('Napaka v podatkih');
+             }*/
+         }
 
-    return (
-        <>
-            <main className="form-signin w-100 m-auto">
-                <h2 className={"error"}>{errorText}</h2>
-                <form onSubmit={submit}>
-                    <h1 className="h3 mb-3 fw-normal">Insert blog</h1>
-                    <div className="form-floating">
-                        <input type="email" className="form-control" id="floatingInput"
-                               placeholder="name@example.com"
-                               onChange={(e) => setTitle(e.target.value)} />
-                        <label htmlFor="floatingInput">Title</label>
-                    </div>
-                    <div className="form-floating">
+        return (
+            <>
+                <main className="form-signin w-100 m-auto">
+                    <h2 className={"error"}>{errorText}</h2>
+                    <form onSubmit={submit}>
+                        <h1 className="h3 mb-3 fw-normal">Insert blog</h1>
+                        <div className="form-floating">
+                            <input type="text" className="form-control" id="floatingInput"
+                                   placeholder="My first blog"
+                                   onChange={(e) => setTitle(e.target.value)}/>
+                            <label htmlFor="floatingInput">Title</label>
+                        </div>
+                        <div className="form-floating">
                         <textarea className="form-control" id="floatingContent"
-                               placeholder="Input content"
-                               onChange={(e) => setContent(e.target.value)}>
+                                  placeholder="Input content"
+                                  style={style}
+                                  rows={10}
+                                  onChange={(e) => setContent(e.target.value)}>
                         </textarea>
-                        <label htmlFor="floatingContent">Input content</label>
-                    </div>
-                    <button className="w-100 btn btn-lg btn-primary" type="submit">Save</button>
-                </form>
-            </main>
-        </>
-    )
-}
+                            <label htmlFor="floatingContent">Input content</label>
+                        </div>
+                        <button className="w-100 btn btn-lg btn-primary" type="submit">Save</button>
+                    </form>
+                </main>
+            </>
+        )
+    }
 
 export default CreateBlog;
